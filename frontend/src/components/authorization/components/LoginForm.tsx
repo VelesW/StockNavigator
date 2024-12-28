d6eab4e8455ba88ae1797960c31c70d9d8b7c061
Piotr Trojan <trojanpiotrek@interia.pl>
2024-12-28 17:17:07 +0100

    Fixed some errors

--- frontend/src/components/authorization/components/LoginForm.tsx
+++ frontend/src/components/authorization/components/LoginForm.tsx
@@ -3,14 +3,44 @@ import AuthInput from "../utils/AuthInput"
 import AuthButton from "../utils/AuthButton"
 import SwitchAuthOption from "../utils/SwitchAuthOption"
 import { FaLock, FaUser } from "react-icons/fa"
+import { useState } from "react"
 
 const LoginForm = () => {
+
+    const [username,setUsername] = useState<string>("")
+    const [password,setPassword] = useState<string>("")
+
+    const handleLogin = () => {
+        
+    }
+
     return (
         <form className="rounded-lg w-72 h-96 text-main-text bg-main-gray shadow-[6px_6px_20px_3px_rgba(0,0,0,0.7)] p-5 flex flex-col justify-center items-center font-exo">
-            <AuthHeader text="Sign in"/>
-            <AuthInput placeholder="Username" icon={<FaUser/>}/>
-            <AuthInput placeholder="Password" icon={<FaLock/>}/>
-            <AuthButton action="LOGIN"/>
+            <AuthHeader 
+                text="Sign in"
+            />
+            <AuthInput
+                type="text"
+                value={username}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    setUsername(e.target.value);
+                }} 
+                placeholder="Username" 
+                icon={<FaUser/>}
+            />
+            <AuthInput 
+                type="password"
+                value={password}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    setPassword(e.target.value);
+                }}
+                placeholder="Password" 
+                icon={<FaLock/>}
+            />
+            <AuthButton 
+                action="LOGIN"
+                handleClick={() => handleLogin()}
+            />
             <SwitchAuthOption 
                 text="Don't have an account?" 
                 linkName="Sign up" 
--- frontend/src/components/authorization/utils/AuthInput.tsx
+++ frontend/src/components/authorization/utils/AuthInput.tsx
@@ -1,21 +1,35 @@
 import { FC, ReactNode } from "react"
 interface AuthInputProps {
+    type: string
+    value: string
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
     placeholder: string
     icon: ReactNode
 }
 
-const AuthInput: FC<AuthInputProps> = ({placeholder, icon}) => {
+const AuthInput: FC<AuthInputProps> = ({
+    type,
+    value,
+    onChange,
+    placeholder,
+    icon,
+}) => {
 
     return (
         <div className="flex flex-row border-b-2 border-main-text mb-2 mt-2">
             <div className="flex items-center justify-center text-emerald-500">
                 {icon}
             </div>
-            <input 
-                placeholder={placeholder}
-                type="text"
-                className="bg-transparent py-2 px-3 focus:outline-none placeholder-main-text"
-            />
+            <div>
+                <input 
+                    placeholder={placeholder}
+                    type={type}
+                    className="bg-transparent py-2 px-3 focus:outline-none placeholder-main-text"
+                    value={value}
+                    onChange={onChange}
+                    required
+                />
+            </div>
         </div>
     )
 }
