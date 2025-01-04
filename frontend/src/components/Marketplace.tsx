b1bf5bd0396ae672bacf40f1aa01c0b2b8ce8398
kacpertokaj <kacpertokajj@gmail.com>
2025-01-04 14:36:01 +0100

    paginate init

--- frontend/src/components/Marketplace.tsx
+++ frontend/src/components/Marketplace.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import { FaChevronLeft } from "react-icons/fa";
+import { FaChevronRight } from "react-icons/fa";
 
 const Marketplace = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 8;
+
   return (
     <div className="w-full flex flex-col">
-      <div className="flex w-full justify-end items-center px-16 gap-8 py-8 bg-zinc-800">
+      <div className="flex w-full justify-end items-center px-16 gap-8 py-8 bg-zinc-800 ring-1 ring-inset ring-gray-500/40">
         <div className="w-full flex justify-center relative">
           <div className="absolute -top-4">
             <input
@@ -22,7 +27,7 @@ const Marketplace = () => {
           Profile
         </a>
       </div>
-      <div className="mt-4 bg-zinc-800 w-full h-full flex flex-col items-center py-16">
+      <div className="mt-4 bg-zinc-800 w-full h-full flex flex-col items-center py-16 ring-1 ring-inset ring-gray-500/40">
         <div className="flex w-[800px] max-w-[95%] flex-col">
           <div className="w-full flex justify-between text-zinc-500 text-sm">
             <div>
@@ -35,7 +40,137 @@ const Marketplace = () => {
             </div>
           </div>
           <div className="flex flex-col">
-            <div className="flex flex-row w-full"></div>
+            <div className="flex flex-row justify-between w-full py-4 px-3">
+              <div className="flex flex-row items-center gap-2">
+                <span>IMG</span>
+                <span className="text-white font-semibold text-sm">
+                  Example
+                </span>
+              </div>
+              <div className="flex gap-8 text-sm">
+                <span className="text-white font-semibold text-sm">1.235</span>
+                <span className="text-white font-semibold text-sm">1.577</span>
+                <span className="text-emerald-500 font-semibold text-sm">
+                  +0.67
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-row justify-between w-full py-4 bg-zinc-900 px-3 rounded-lg">
+              <div className="flex flex-row items-center gap-2">
+                <span>IMG</span>
+                <span className="text-white font-semibold text-sm">
+                  Example
+                </span>
+              </div>
+              <div className="flex gap-8 text-sm">
+                <span className="text-white font-semibold text-sm">1.235</span>
+                <span className="text-white font-semibold text-sm">1.577</span>
+                <span className="text-emerald-500 font-semibold text-sm">
+                  +0.67
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-row justify-between w-full py-4 px-3">
+              <div className="flex flex-row items-center gap-2">
+                <span>IMG</span>
+                <span className="text-white font-semibold text-sm">
+                  Example
+                </span>
+              </div>
+              <div className="flex gap-8 text-sm">
+                <span className="text-white font-semibold text-sm">1.235</span>
+                <span className="text-white font-semibold text-sm">1.577</span>
+                <span className="text-red-500 font-semibold text-sm">
+                  -0.67
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-row justify-between w-full py-4 bg-zinc-900 px-3 rounded-lg">
+              <div className="flex flex-row items-center gap-2">
+                <span>IMG</span>
+                <span className="text-white font-semibold text-sm">
+                  Example
+                </span>
+              </div>
+              <div className="flex gap-8 text-sm">
+                <span className="text-white font-semibold text-sm">1.235</span>
+                <span className="text-white font-semibold text-sm">1.577</span>
+                <span className="text-red-500 font-semibold text-sm">
+                  -0.67
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-row justify-between w-full py-4 px-3">
+              <div className="flex flex-row items-center gap-2">
+                <span>IMG</span>
+                <span className="text-white font-semibold text-sm">
+                  Example
+                </span>
+              </div>
+              <div className="flex gap-8 text-sm">
+                <span className="text-white font-semibold text-sm">1.235</span>
+                <span className="text-white font-semibold text-sm">1.577</span>
+                <span className="text-red-500 font-semibold text-sm">
+                  -0.67
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-row justify-between w-full py-4 bg-zinc-900 px-3 rounded-lg">
+              <div className="flex flex-row items-center gap-2">
+                <span>IMG</span>
+                <span className="text-white font-semibold text-sm">
+                  Example
+                </span>
+              </div>
+              <div className="flex gap-8 text-sm">
+                <span className="text-white font-semibold text-sm">1.235</span>
+                <span className="text-white font-semibold text-sm">1.577</span>
+                <span className="text-red-500 font-semibold text-sm">
+                  -0.67
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-row justify-between w-full py-4 px-3">
+              <div className="flex flex-row items-center gap-2">
+                <span>IMG</span>
+                <span className="text-white font-semibold text-sm">
+                  Example
+                </span>
+              </div>
+              <div className="flex gap-8 text-sm">
+                <span className="text-white font-semibold text-sm">1.235</span>
+                <span className="text-white font-semibold text-sm">1.577</span>
+                <span className="text-red-500 font-semibold text-sm">
+                  -0.67
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-row justify-between w-full py-4 bg-zinc-900 px-3 rounded-lg">
+              <div className="flex flex-row items-center gap-2">
+                <span>IMG</span>
+                <span className="text-white font-semibold text-sm">
+                  Example
+                </span>
+              </div>
+              <div className="flex gap-8 text-sm">
+                <span className="text-white font-semibold text-sm">1.235</span>
+                <span className="text-white font-semibold text-sm">1.577</span>
+                <span className="text-red-500 font-semibold text-sm">
+                  -0.67
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex w-[800px] max-w-[95%] flex-col mt-12">
+          <div className="w-full flex items-center justify-center">
+            <button className="text-white py-2 px-2 bg-emerald-500 rounded-lg">
+              <FaChevronLeft />
+            </button>
+            <span className="px-4 text-white text-sm">1 2 .. 4</span>
+            <button className="text-white py-2 px-2 bg-emerald-500 rounded-lg">
+              <FaChevronRight />
+            </button>
           </div>
         </div>
       </div>
