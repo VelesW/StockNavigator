450f653615f97b0d82a7e9878a004405df285051
kacpertokaj <kacpertokajj@gmail.com>
2025-01-25 21:44:31 +0100

    cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import DefaultPage from "./pages/DefaultPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import MainPage from "./pages/MainPage";
-import PrivateRoute from "./routes/PrivateRoute";
 
 function App() {
   return (
@@ -14,9 +13,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
 
-          {/* <Route element={<PrivateRoute />}> */}
           <Route path="/main" element={<MainPage />} />
-          {/* </Route> */}
         </Routes>
       </Router>
     </>
